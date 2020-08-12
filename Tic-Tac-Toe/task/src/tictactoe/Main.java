a8b1a1a62f1d42c5f997d0e05b02b79958d28c6c
PrzemekZacheja <60814241+PrzemekZacheja@users.noreply.github.com>
2020-08-12 13:51:23 +0200

    pierwszy etap: wypisanie wszystkich stanów gry
    Signed-off-by: PrzemekZacheja <60814241+PrzemekZacheja@users.noreply.github.com>

--- Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -27,54 +27,70 @@ public class Main {
         }
 
         String state = "Game not finished";
-        String stateX;
-        String stateO;
+
+
+        String stateX = null;
+        String stateO = null;
 //        while (state == "Game not finished") {
 
         if ((ticTac[0] == 'X') && (ticTac[1] == 'X') && (ticTac[2] == 'X')) {
-            state = "X wins";
-        } else if ((ticTac[0] == 'O') && (ticTac[1] == 'O') && (ticTac[2] == 'O')) {
-            state = "O wins";
+            stateX = "X wins";
         } else if ((ticTac[3] == 'X') && (ticTac[4] == 'X') && (ticTac[5] == 'X')) {
-            state = "X wins";
-        } else if ((ticTac[3] == 'O') && (ticTac[4] == 'O') && (ticTac[5] == 'O')) {
-            state = "O wins";
+            stateX = "X wins";
         } else if ((ticTac[6] == 'X') && (ticTac[7] == 'X') && (ticTac[8] == 'X')) {
-            state = "X wins";
-        } else if ((ticTac[6] == 'O') && (ticTac[7] == 'O') && (ticTac[8] == 'O')) {
-            state = "O wins";
+            stateX = "X wins";
         } else if ((ticTac[0] == 'X') && (ticTac[3] == 'X') && (ticTac[6] == 'X')) {
-            state = "X wins";
-        } else if ((ticTac[0] == 'O') && (ticTac[3] == 'O') && (ticTac[6] == 'O')) {
-            state = "O wins";
-        } else if ((ticTac[1] == 'O') && (ticTac[4] == 'O') && (ticTac[7] == 'O')) {
-            state = "O wins";
+            stateX = "X wins";
         } else if ((ticTac[1] == 'X') && (ticTac[4] == 'X') && (ticTac[7] == 'X')) {
-            state = "X wins";
+            stateX = "X wins";
         } else if ((ticTac[2] == 'X') && (ticTac[5] == 'X') && (ticTac[8] == 'X')) {
-            state = "X wins";
-        } else if ((ticTac[2] == 'O') && (ticTac[5] == 'O') && (ticTac[8] == 'O')) {
-            state = "O wins";
+            stateX = "X wins";
         } else if ((ticTac[0] == 'X') && (ticTac[4] == 'X') && (ticTac[8] == 'X')) {
-            state = "X wins";
-        } else if ((ticTac[0] == 'O') && (ticTac[4] == 'O') && (ticTac[8] == 'O')) {
-            state = "O wins";
+            stateX = "X wins";
         } else if ((ticTac[2] == 'X') && (ticTac[4] == 'X') && (ticTac[6] == 'X')) {
-            state = "X wins";
-        } else if ((ticTac[2] == 'O') && (ticTac[4] == 'O') && (ticTac[6] == 'O')) {
-            state = "O wins";
-        } else {
-            state = "Draw";
-        }
-        for (int i = 0; i < ticTac.length; i++) {
-            if (ticTac[i] == '_') {
-                if (state != "X wins" && state != "O wins") {
-                    state = "Game not finished";
-                    break;
-                }
-            }
+            stateX = "X wins";
         }
 
+        if ((ticTac[0] == 'O') && (ticTac[1] == 'O') && (ticTac[2] == 'O')) {
+            stateO = "O wins";
+        } else if ((ticTac[3] == 'O') && (ticTac[4] == 'O') && (ticTac[5] == 'O')) {
+            stateO = "O wins";
+        } else if ((ticTac[6] == 'O') && (ticTac[7] == 'O') && (ticTac[8] == 'O')) {
+            stateO = "O wins";
+        } else if ((ticTac[0] == 'O') && (ticTac[3] == 'O') && (ticTac[6] == 'O')) {
+            stateO = "O wins";
+        } else if ((ticTac[1] == 'O') && (ticTac[4] == 'O') && (ticTac[7] == 'O')) {
+            stateO = "O wins";
+        } else if ((ticTac[2] == 'O') && (ticTac[5] == 'O') && (ticTac[8] == 'O')) {
+            stateO = "O wins";
+        } else if ((ticTac[0] == 'O') && (ticTac[4] == 'O') && (ticTac[8] == 'O')) {
+            stateO = "O wins";
+        } else if ((ticTac[2] == 'O') && (ticTac[4] == 'O') && (ticTac[6] == 'O')) {
+            stateO = "O wins";
+        }
+
+        if (stateX != null && stateO == null) {
+            state = stateX;
+        } else if (stateX == null && stateO != null) {
+            state = stateO;
+        } else if (stateX != null && stateO != null) {
+            state = "Impossible";
+        } else if (stateX == null && stateO == null) {
+            state = "Draw";
+            for (int i = 0; i < ticTac.length; i++) {
+                if (ticTac[i] == '_') {
+                    if (state != "X wins" && state != "O wins") {
+                        state = "Game not finished";
+                        break;
+                    }
+                }
+            }
+
+        }
+
+        if (countX - countO > 1 || countO - countX > 1) {
+            state = "Impossible";
+        }
 
         System.out.println(state);
     }
